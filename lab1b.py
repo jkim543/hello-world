2811c6d7b46b160e198e04990636296a7e8aabdf
jkim543 <65576955+jkim543@users.noreply.github.com>
2020-05-22 21:20:37 -0400

    Add files via upload
    jkim543 lab1 submission

---
+++ lab1b.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+name = 'Isaac'
+
+print ("How old are you "+ name +"?") 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ lab1d.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+x = 10
+y = 2
+z = 5
+
+print( '10 + 2 * 5 =' , x + y * z )
+
+
+
